ebd6cb05f5cb761eddd729245f24988ae2172aa6
Kamil Politowicz <kamilp@hey.com>
2024-06-09 11:36:00 -0400

    Day 1, part 2

--- 01/calibration_value.exs
+++ 01/calibration_value.exs
@@ -5,4 +5,35 @@ defmodule CalibrationValue do
 
     Integer.parse(first <> last) |> elem(0)
   end
+
+  def extract_from_complex_string(str) do
+    matching = "[1-9]|one|two|three|four|five|six|seven|eight|nine"
+
+    first = Regex.named_captures(~r/^.*?(?<match>#{matching}).*$/, str)["match"]
+    last = Regex.named_captures(~r/^.*(?<match>#{matching}).*?$/, str)["match"]
+
+    res =
+    to_int(first) <> to_int(last)
+    |> Integer.parse()
+    |> elem(0)
+
+    # IO.inspect({str, res, first, last})
+
+    res
+  end
+
+  defp to_int(str) do
+    case str do
+      "one" -> "1"
+      "two" -> "2"
+      "three" -> "3"
+      "four" -> "4"
+      "five" -> "5"
+      "six" -> "6"
+      "seven" -> "7"
+      "eight" -> "8"
+      "nine" -> "9"
+      _ -> str
+    end
+  end
 end
--- 01/calibration_value_test.exs
+++ 01/calibration_value_test.exs
@@ -11,4 +11,17 @@ defmodule CalibrationValueTest do
     assert 15 = CalibrationValue.extract_from_string("a1b2c3d4e5f")
     assert 77 = CalibrationValue.extract_from_string("treb7uchet")
   end
+
+  test "extracts calibration value from a string with numbers spelled out" do
+    assert 29 = CalibrationValue.extract_from_complex_string("two1nine")
+    assert 83 = CalibrationValue.extract_from_complex_string("eightwothree")
+    assert 13 = CalibrationValue.extract_from_complex_string("abcone2threexyz")
+    assert 24 = CalibrationValue.extract_from_complex_string("xtwone3four")
+    assert 42 = CalibrationValue.extract_from_complex_string("4nineeightseven2")
+    assert 14 = CalibrationValue.extract_from_complex_string("zoneight234")
+    assert 76 = CalibrationValue.extract_from_complex_string("7pqrstsixteen")
+    assert 99 = CalibrationValue.extract_from_complex_string("9hcj")
+    assert 21 = CalibrationValue.extract_from_complex_string("twone")
+    assert 41 = CalibrationValue.extract_from_complex_string("4seven1smptmjbfsztwo6eighttwotwonevcv")
+  end
 end
